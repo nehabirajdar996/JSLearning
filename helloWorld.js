34182ee3059a97a5847783f5a82332303a4088da
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-02-16 18:28:12 +0530

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,22 @@
 console.log("neha birajdar");
+console.log("26");
+console.log("pune");
+
+var fullName="neha birajdar"
+console.log(fullName);
+console.log(fullName);
+
+var city; //variable declration
+console.log(city); 
+city="pune";//variable initialization
+console.log(city);
+city="surat";
+console.log(city);
+
+var age ="26"//variable declaration and intialzation in same line
+age=32;
+age = 33 ;
+console.log(age);
+
+var university; //variable declation
+university="solapur"//variable initialization
