67c46c896513c503f213912bec870b43ea0f73ae
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-02-17 13:47:21 +0530

    Done with Assignment 03 (17 feb)
    Done with Assignment 03 (17 feb)

---
+++ 02_swapVariables.js
@@ -0,0 +1,31 @@
+console.log("******* step1: swap two variables value ******");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("* Before swapping *");
+console.log("sweety:",sweety);
+console.log("cutie:",cutie);
+
+console.log("* After swapping *");
+var temp;
+temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("sweety:",sweety);
+console.log("cutie:",cutie);
+
+console.log("******* step2: swap three variables value ******");
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("* Before swapping *");
+console.log("num1:",num1, "num2:",num2,"num3:",num3);
+
+var temp;
+temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("* After swapping *");
+console.log("num1:",num1, "num2:",num2,"num3:",num3);
+
+
