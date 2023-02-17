d875403d9993dc61958b2404aa951f1ee99df824
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-02-17 13:44:58 +0530

    done with assignment 01 (17feb) for variables and null vs undefined
    done with assignment 01 (17feb) for variables and null vs undefined

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,33 @@
+console.log("*******step1*******");
+console.log(" # Number #");// 3 variables for number
+var phoneNumber=8975897540;
+console.log("1. PhoneNo.:",phoneNumber, "Type:",typeof phoneNumber);
+var pincode=411033;
+console.log("2. PIN:",pincode, "Type:",typeof pincode);
+var marks=80;
+console.log("3. Marks:",marks, "Type:",typeof marks);
+
+console.log(" # String #");
+var firstname="Neha";
+console.log("1. firstNmae:",firstname, "Type:",typeof firstname);
+var middleName="Ganesh";
+console.log("2.middleNmae:",middleName,"Type:",typeof middleName);
+var lastName="Koli";
+console.log("3. lastNmae:",lastName,   "Type:",typeof lastName);
+
+console.log(" # Boolean #");
+var isGraduated=true;
+console.log("1. isGradated:",isGraduated, "Type:",typeof isGraduated);
+var isPostgraduated=false;
+console.log("2. isPostgradated:",isPostgraduated,"Type:",typeof isPostgraduated);
+var isMarried= true;
+console.log("3. isMarried:",isMarried, "Type:",typeof isMarried);
+
+console.log("*******step2*******");
+var num1=null;
+console.log("value of num1 is:",num1,"and","Type of num1 is:",typeof num1);
+var num2;
+console.log("value of num1 is:",num2,  "and" ,"Type of num1 is:",typeof num2s);
+ 
+
+
