dc7afc12ec7d3c20faba3f5cc62a1adee6f4f76e
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-02-17 13:46:32 +0530

    done with Assignment 02 (17 FEB) Variable,datatype  and typeOf oprerator
    done with Assignment 02 (17 FEB) Variable,datatype  and typeOf oprerator

---
+++ 02_typeOf.js
@@ -0,0 +1,32 @@
+console.log("**************** step1 *************");
+var bankName;
+console.log("# before intilization of variable bankName #");
+console.log("value of bankName:",bankName, '::::', "Datatype of bankName:",  typeof bankName );
+bankName="SBI Bank";
+console.log("# After intilization of variable #");
+console.log("value of bankName:",bankName, '::::', "Datatype of bankName:", typeof bankName );
+
+console.log("**************** step2 *************");
+var marks="90%"
+console.log("value of marks is :",marks, '::::',"Datatype of mark is:",typeof marks);
+
+console.log("***************** step3 ************");
+var isWorking;
+console.log("# before intilization of variable isWorking #");
+console.log("value of isWorking:",isWorking, '::::', "Datatype of isWorking:",typeof is );
+isWorking=true;
+console.log("# After intilization of variable isWorking #");
+console.log("value of isWorking:",isWorking, "::::", "Datatype of isWorking:", typeof isWorking );
+
+console.log("***************** step4 ************");
+var totalCount="Hundred and Seven";
+console.log(" a. value of totalCount:",totalCount, '::::',"Dataype of totalCount:", typeof totalCount );
+totalCount=107;
+console.log(" a. value of totalCount:",totalCount, '::::',"Dataype of totalCount:", typeof totalCount );
+
+
+
+
+
+
+
