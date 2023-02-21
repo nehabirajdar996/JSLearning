9e03adacc3489fff36e71c1fab6c4e2fe6f9103b
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-02-21 17:08:13 +0530

    basic string assignment1 (21 feb)
    basic string assignment1 (21 feb)

---
+++ strbasicassig_01.js
@@ -0,0 +1,13 @@
+console.log("************* step 1 ************");
+function stringBasics(){
+    console.log("My dream company is: 'GOOGLE'");
+}
+stringBasics();
+
+console.log("************* step 2 ************");
+var hobby1="Reading";
+var hobby2="singing";
+var hobby3="Drawing";
+console.log( "2.1.","Hobby1:",hobby1," ","Hobby2:",hobby2," ","Hobby3:",hobby3);
+var sum= hobby1.length + hobby2.length + hobby3.length;
+console.log("2.2.","sum of total number of characters available in hobby1,hobby2,hobby3:",sum);
