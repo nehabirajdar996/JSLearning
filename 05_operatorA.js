e228f81e9be551de36cccbba121f0e7a147e4f24
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-02-22 17:07:03 +0530

    operation assignment 05(22feb)
    operation assignment 05(22feb)

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -1,22 +1,28 @@
 console.log(`*************** step 1 *************`);
 function squareOfWordLength(value) {
-    //var wordLength = value;
-    console.log(`> Length of word "${value}": ${value.length}  
->> Length of square of word "${value}": ${value.length**2} `);
+    var wordLength = value.length;
+    console.log(`> Length of word "${value}" is: ${value.length}  `);
+    var sqrOfWord=wordLength ** 2;
+    return sqrOfWord;
+
     
 }
-squareOfWordLength("JavaScript"); 
-squareOfWordLength("Google Chrome");
-squareOfWordLength("Developer Smart");
+result1=squareOfWordLength("JavaScript");
+console.log(`The square of length is: ${result1}`);
+result2=squareOfWordLength("Google Chrome");
+console.log(`The square of length is: ${result2}`);
+result3=squareOfWordLength("Developer Smart");
+console.log(`The square of length is: ${result3}`);
 
 console.log(`*************** step 2 *************`);
 var givenString = "I am Angular Developer";
 function string(){
     var strLen=givenString.length;
-    var dividStr=givenString.split(" ");
-    console.log(`2.1  Length of string is: ${strLen} String after division: [${dividStr}]`);
-    var totalWordStr=strLen*dividStr.length;
-    console.log(`2.2  Multiplication of string length "${strLen}" and total words "${dividStr.length}" available in string is: ${totalWordStr}`);
+    var splitStr=givenString.split(" ");
+    var dividStr=strLen/splitStr.length;
+    console.log(`2.1  Length of string is: ${strLen}   String after division: ${dividStr}`);
+    var totalWordStr=strLen*splitStr.length;
+    console.log(`2.2  Multiplication of string length "${strLen}" and total words "${splitStr.length}" available in string is: ${totalWordStr}`);
 
 }
 string();
