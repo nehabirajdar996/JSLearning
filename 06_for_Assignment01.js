90962c5ab44c6037e047103b2d478ce4075a561a
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-03-01 11:55:26 +0530

    Completed 06_for_Assignment 01 (1 March)
    Completed 06_for_Assignment 01 (1 March)

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,61 @@
+console.log(` *** 1.WAP to print numbers from 5 to 15 by incrementing 1 ***`);
+//intialization = index = 5 ;
+//condition = index<=15;
+//update expression = index=index+1;
+for (let index = 5; index <= 15; index=index+1) {
+   console.log(`${index}`);
+}
+
+console.log(` *** 2.WAP to print numbers from 50 to 40 by decrementing 1 ***`);
+//intialization = index = 50 ;
+//condition = index >= 40;
+//update expression = index=index-1;
+for (let index = 50; index >=40; index=index-1) {
+    console.log(index);
+    
+}
+
+console.log(` *** 3.WAP to find first "15" "odd" numbers ***`);
+//intialization = index = 1 ;
+//condition = index <= 30;
+//update expression = index = index + 2;
+for (let index = 1; index <= 30; index  =index + 2) {
+    console.log(index);
+    
+}
+
+console.log(` *** 4.WAP to find first "10" "EVEN" numbers ***`);
+//intialization = index = 0 ;
+//condition = index <= 20;
+//update expression = index = index + 2;
+for (let index = 0; index <= 20; index  =index + 2) {
+    console.log(index);
+    
+}
+
+console.log(` *** 5.WAP to print table of 5 likes --> 5 10 15 20 15 ........50 ***`);
+//intialization = index = 5 ;
+//condition = index <= 50;
+//update expression = index = index + 5;
+for (let index = 5; index <= 50; index  =index + 5) {
+    console.log(index);
+    
+}
+
+console.log(` *** 6.WAP to print table of 10 likes --> 10 20 30 40 ........100 ***`);
+//intialization = index = 10;
+//condition = index <= 100;
+//update expression = index = index + 10;
+for (let index = 10; index <= 100; index  =index + 10) {
+    console.log(index);
+    
+}
+
+console.log(` *** 7.WAP to print table of 10 in revarse likes --> 100 90 80 70  ........10 ***`);
+//intialization = index = 100;
+//condition = index >= 10;
+//update expression = index = index - 10;
+for (let index = 100; index >= 10; index  =index - 10) {
+    console.log(index);
+    
+}
