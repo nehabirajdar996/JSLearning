45c37572553090c3c2952f0d93495e211809b66d
nehabirajdar996 <neha.birajdar996@gmail.com>
2023-03-09 17:39:15 +0530

    completed assignment "D" on object (9 march)
    completed assignment "D" on object (9 march)

---
+++ 09_object_assigE.js
@@ -0,0 +1,50 @@
+let sbiBank={
+    bankName:"SBI Bank",
+    location:"Mumbai",
+    accountNo:"2502682014",
+    ifsc:"sbii05678945",
+    interestRate:"6%",
+    
+    showDetails:function(){
+        console.log(sbiBank);
+    }
+
+}
+sbiBank.showDetails()
+
+let axisBank={
+    bankName:"AXIS BANK",
+    location:"PUNE",
+    accountNo:"24589224763",
+    ifsc:"axis0125798",
+    interestRate:"5.5",
+    showDetails:function(){
+        console.log(axisBank);
+    }
+}
+axisBank.showDetails()
+
+let hdfcBank={
+    bankName:"HDFC Bank",
+    location:"Solapur",
+    accountNo:"458965732",
+    ifsc:"hdfc0387896",
+    interestRate:"5.8%",
+    showDetails:function(){
+        console.log(hdfcBank);
+    }
+}
+hdfcBank.showDetails()
+
+let yesBank={
+    bankName:"YES Bank",
+    location:"Nashik",
+    accountNo:"568943569875",
+    ifsc:"yess0245896",
+    interestRate:"6.2%",
+    showDetails:function(){
+        console.log(sbiBank);
+    }
+}
+yesBank.showDetails()
+
